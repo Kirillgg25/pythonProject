9f7cd1b6acfb2f27f58cf40a143baf6f96932552
Kirillgg25 <103053339+Kirillgg25@users.noreply.github.com>
2025-04-01 17:31:46 +0200

    Delete 5lesdz.py

--- 5lesdz.py
+++
@@ -1,8 +0,0 @@
-from colorama import Fore, Back, Style # Наши модули
-
-print(Fore.RED + "Hi") # Fore - цвет текста
-print(Back.BLACK + "Hi") # Back - цвет фона
-print(Style.DIM + "Hi") # DIM - делает цвет тусклее
-print("Hi") # Как можем заметить настройки сохраняються так что нам ножно сбросить командой ниже
-print(Style.RESET_ALL)
-print("Hi")
